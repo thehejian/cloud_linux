387963106236758e48adf64fff2bdfc6f87020f6
thehejian <745544752@qq.com>
2021-10-30 21:54:58 +0800

    012 注释中文

--- 012passwd_v3.0.sh
+++ 012passwd_v3.0.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-read -p "input user " user
+read -p "input username " user
+#输入用户名，并将用户名保存为变量
 
 if [ -z $user ] ; then
 	echo "input username "
 	exit
 fi
+#-z 是否存在
+
 useradd $user 
+
 stty -echo
+#关闭屏幕显示
 read -p "input passwd" passWd
 stty echo
 
