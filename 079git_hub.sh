843b35abbacdc457c10bd298bf943f163d723a73
thehejian <745544752@qq.com>
2021-10-30 13:18:29 +0800

    20211030 13:18

--- 079git_hub.sh
+++ 079git_hub.sh
@@ -3,7 +3,6 @@
 mail=745544752@qq.com
 ssh-keygen -t ed25519 -C "$mail"
 
-rm -rf /cloudnas/cloudnas/CloudDrive/阿里云盘/001-脚本/id_ed25519.pub
 cp /root/.ssh/id_ed25519.pub /cloudnas/cloudnas/CloudDrive/阿里云盘/001-脚本/
 #jiang gongyao fuzhi dao nginx de html mulu;
 
@@ -16,16 +15,19 @@ ssh -T git@github.com
 git config --global user.name "thehejian"
 git config --global user.email "745544752@qq.com"
 
-
-cd /nas/001-脚本
+cd ~/sh_github/
 
 #git remote rm origin
 echo "# cloud_linux" >> README.md
 git init
+git add .
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/thehejian/cloud_linux.git
+
+git remote add origin git@github.com:thehejian/cloud_linux.git
+#git remote add origin https://github.com/thehejian/cloud_linux.git
+#https de lianbushang o
 git push -u origin main
 #diyici jia -u
 
