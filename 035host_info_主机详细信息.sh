80107b76e03c51e053d3914833442c5c0c26ab43
thehejian <745544752@qq.com>
2021-11-01 09:03:58 +0800

    Update 035host_info_主机详细信息.sh
    202111010903

--- 035host_info_主机详细信息.sh
+++ 035host_info_主机详细信息.sh
@@ -8,18 +8,31 @@ wangka=eth0
 while :
 do
 date
-uptime | awk '{print "CPU负载是 " $8,$9,$10}'
+uptime | awk -F[,] '{print "CPU负载是 " $4,$5,$6}'
+# 平均负载量：最近1分钟、5分钟、15分钟系统
+
+
 free -h | awk '/^内存/{print "内存剩余 " $4}'
+
 df -h | awk '/\/$/{print "根存储空间可用 " $4}'
+
 ifconfig $wangka | awk '/RX packets/{print "网卡 "$wangka" 流入量 " $5/10^6 "MB"}'
+#网卡流入量
+
 ifconfig $wangka | awk '/TX packets/{print "网卡 "$wangka" 流出量 " $5/10^6 "MB"}'
-echo "安装包共 $(dpkg -l | wc -l) 个"
-#echo "安装包共 $(rpm -qa | wc -l) 个"
-#echo "本机安装软件包数量 $(cat /etc/passwd | wc -l) 个"
+#网卡流出量
+
+#echo "安装包共 $(dpkg -l | wc -l) 个"
+echo "安装包共 $(rpm -qa | wc -l) 个"
+
+#echo "本机用户数 $(cat /etc/passwd | wc -l) 个"
 awk 'END{print "本机用户数 " NR " 个"}' /etc/passwd
+
 echo "当前登录人数 $(who | wc -l)"
+
 echo "当前进程数 $(ps -ef | wc -l)"
 #echo "当前进程数 $(ps aux | wc -l)"
+
 sleep 20
 clear
 done
