0a56875dba2869d18106445f8e7388555edc883c
thehejian <745544752@qq.com>
2021-11-01 21:01:23 +0800

    Update and rename 037host_login_v2.sh to 037host_login_v2_查找IP.sh
    20211101 21:01

--- 037host_login_v2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk '/Failed password for root/{print $11}' /var/log/secure | awk '{ip[$1]++}END{for(i in ip){print ip[i],i}}' | awk '$1>3{print "入侵" $1 "次\t","ip 为" $2}' | sort -r
---
+++ 037host_login_v2_查找IP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+awk '/Failed password for root/{print $11}' /var/log/secure | awk '{ip[$1]++}END{for(i in ip){print ip[i],i}}' | awk '$1>3{print "入侵" $1 "次\t","ip 为" $2}' | sort -r
+#找到IP行
+#awk '{ip[$1]++}END{for(i in ip){print ip[i],i}}'
+  #逐条执行ip，并自增计数；——》ip[i]是数组的序号，不是值——》进行自增统计；
+  #i是上一个$11 ip
+#awk '$1>3{print "入侵" $1 "次\t","ip 为" $2}——》$1是上一个数组的序号，$2是上一个IP
+#sort -r 排倒叙
