8f258ab032794c99b0eaf1107121fe3536b47bea
thehejian <745544752@qq.com>
2021-11-01 08:52:03 +0800

    Update and rename 035host_info.sh to 035host_info_主机详细信息.sh

--- 035host_info_主机详细信息.sh
+++ 035host_info_主机详细信息.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#查看本机的：CPU负载、内存容量、存储空间、网卡流入量、网卡流出量、安装软件包数量、用户数、当前登录人数、当前进程数
+#每20s执行一次
+
+wangka=eth0
+#设置网卡名称为变量
 
 while :
 do
@@ -6,12 +11,12 @@ date
 uptime | awk '{print "CPU负载是 " $8,$9,$10}'
 free -h | awk '/^内存/{print "内存剩余 " $4}'
 df -h | awk '/\/$/{print "根存储空间可用 " $4}'
-ifconfig ens33 | awk '/RX packets/{print "网卡ens33流入量 " $5/10^6 "MB"}'
-ifconfig ens33 | awk '/TX packets/{print "网卡ens33流出量 " $5/10^6 "MB"}'
+ifconfig $wangka | awk '/RX packets/{print "网卡 "$wangka" 流入量 " $5/10^6 "MB"}'
+ifconfig $wangka | awk '/TX packets/{print "网卡 "$wangka" 流出量 " $5/10^6 "MB"}'
 echo "安装包共 $(dpkg -l | wc -l) 个"
 #echo "安装包共 $(rpm -qa | wc -l) 个"
 #echo "本机安装软件包数量 $(cat /etc/passwd | wc -l) 个"
-awk 'END{print "本机安装软件包数量 " NR " 个"}' /etc/passwd
+awk 'END{print "本机用户数 " NR " 个"}' /etc/passwd
 echo "当前登录人数 $(who | wc -l)"
 echo "当前进程数 $(ps -ef | wc -l)"
 #echo "当前进程数 $(ps aux | wc -l)"
