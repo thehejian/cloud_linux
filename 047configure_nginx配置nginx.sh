53307f54de6be02db585789ef674656837e71213
thehejian <745544752@qq.com>
2021-11-02 09:06:13 +0800

    Update and rename 047configure_nginx.sh to 047configure_nginx配置nginx.sh

--- 047configure_nginx配置nginx.sh
+++ 047configure_nginx配置nginx.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
+
+#设置访问密码、开启目录访问、https设置
+
 #vim /usr/local/nginx/conf/nginx.conf
 #/usr/local/nginx/html/
 
-cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak
+mydate=date +'%Y%m%d'
+cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.$mydate.bak
 #备份conf文件
 
 egrep -v '^$|^.*#' /usr/local/nginx/conf/nginx.conf.bak > /usr/local/nginx/conf/nginx.conf
 #空白行和注释的去掉
 
+####################################################################################################————》设置访问密码
 sed -i '12a \
         auth_basic "input password"; \
         auth_basic_user_file "/usr/local/nginx/pass";' /usr/local/nginx/conf/nginx.conf
 
-#authentication prompt message
-#authenticationpasswordfile
+#设置auth_basic
+#设置auth_basic_user_file
 
-
+#密码功能需要安装--with-http_ssl_module
 yum install -y httpd-tools
 
 #echo "input password"
 #htpasswd -c /usr/local/nginx/pass hejian
+#交互方式设置，不方便
 touch /usr/local/nginx/pass
 htpasswd -b /usr/local/nginx/pass hejian 123456
+#非交互方式设置htpass的密码文件
 
+####################################################################################################————》开启访问目录
 sed -i '12a \
         autoindex on; \
         charset utf-8;' /usr/local/nginx/conf/nginx.conf
+#开启访问目录
 
 sed -i '/server_name/s/localhost/www.hejian.com/g' /usr/local/nginx/conf/nginx.conf
+#修改虚拟主机名称
 
 echo "1.116.26.230 www.hejian.com" >> /etc/hosts
+#host添加域名解析
 
+####################################################################################################————》https设置
+
+##################step1创建私钥
 openssl genrsa > /usr/local/nginx/conf/cert.key 
 #generate private key
 #创建私钥
 
+##################step2创建公钥（证书）（通过私钥创建）
 openssl req -new -x509 -key /usr/local/nginx/conf/cert.key  > /usr/local/nginx/conf/cert.pem 
-#generate public key (certificate) 创建公钥（证书）
+#generate public key (certificate) 
+#创建公钥（证书）
 
     #server {
     #    listen       443 ssl;
@@ -57,11 +73,6 @@ openssl req -new -x509 -key /usr/local/nginx/conf/cert.key  > /usr/local/nginx/c
     #    } 
     #    }
 # 上面的文件复制到  /usr/local/nginx/conf/nginx.conf 下，第9行开始；sed用法搞不定
-
-
-#authentication prompt message
-#authenticationpasswordfile
-
 
 killall -9 nginx
 #nginx进程杀掉后再重启
