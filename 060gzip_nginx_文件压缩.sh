d0cc64f1461a077741681539f99a3cc1f01ed031
thehejian <745544752@qq.com>
2021-11-02 15:54:28 +0800

    Update and rename 060gzip_nginx.sh to 060gzip_nginx_文件压缩.sh

--- 060gzip_nginx_文件压缩.sh
+++ 060gzip_nginx_文件压缩.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+#针对css等文件可以进行压缩
 sed -i '7a \
      gzip on; \
      gzip_min_length 1000; \
      gzip_comp_level 4; \
      gzip_types text/plain text/css application/javascript application/x-javascript;' /usr/local/nginx/conf/nginx.conf
 
-#gzip_min_length 1000 ;xiao wenjian bu yasuo
-#mime_type /usr/local/nginx/conf/mime.types
-
-
+#gzip_min_length 1000 ;小于1000的不压缩
+#mime_type /usr/local/nginx/conf/mime.types 全部文件格式的路径
 
 systemctl reload nginx
