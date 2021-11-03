755ec8ce679b56233674d4fcc5bab65d51a5af01
thehejian <745544752@qq.com>
2021-11-03 18:02:29 +0800

    Update and rename 077git_server_git.sh to 077git_server_git方法_服务器端.sh

--- 077git_server_git方法_服务器端.sh
+++ 077git_server_git方法_服务器端.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#################################################
-#git fangfa
+
+#git 方法
 
 yum -y install git-daemon
-#read server
-#buxuyao ssh
+#不需要ssh
 sed -i 's#--base-path=/var/lib/git#--base-path=/var/git#g' /usr/lib/systemd/system/git@.service
 
 systemctl start git.socket
 systemctl enable git.socket
 systemctl status git.socket
 
-#change to client
+##################################################客户端
 cd~
 #rm -rf project
 #xuyao shoudong qiehuan mulu
 
 git clone git://1.116.26.230/project
+#git方法获取，好像需要私钥和证书配合，github是这样
+#ssh方法获取：git clone root@1.116.26.230:/var/git/project
