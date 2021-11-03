47df9f8190ef4707f182eaf4b4174a67610acfd8
thehejian <745544752@qq.com>
2021-11-03 16:32:10 +0800

    Update and rename 071varnish_proxy.sh to 071varnish_proxy代理服务器介绍.sh

--- 071varnish_proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#varnish shi proxy bushi web;
-#you huancun gongneng;
-#client ---varnish --- web1(a.mp3);
-#varnish hui baocun;
---
+++ 071varnish_proxy代理服务器介绍.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#varnish 代理服务器，不是web服务器;
+#有缓存功能g;
+#client ---varnish --- web1(a.mp3);
+#varnish 缓存服务器内容;
