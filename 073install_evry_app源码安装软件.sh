c75b1fd49b6365ddcd25f99c431265d22fc9acd6
thehejian <745544752@qq.com>
2021-11-03 17:07:39 +0800

    Update and rename 073install_evry_app.sh to 073install_evry_app源码安装软件.sh

--- 073install_evry_app源码安装软件.sh
+++ 073install_evry_app源码安装软件.sh
@@ -4,14 +4,15 @@ app=git
 version=2.33.1
 path=/usr/local/$app/
 appurl=https:/git.kernel.org/pub/scm/git/git.git/snapshot
-#buyao app version de url; buyao /;
+#不带版本的url; 不要/;
 
 yum -y install gcc readline-devel
 yum -y install ncurses-devel
 yum -y install pcre-devel
 yum -y install python-docutils
-#varnish de yilai bao
+#varnish 的依赖包
 
+#创建不登录的用户
 useradd -s /sbin/nologin $app
 
 mkdir -p $path
@@ -23,6 +24,8 @@ tar -xf $app-$version.tar.gz
 
 cd $app-$version/
 
+#测试存在的特性，检测依赖
 ./configure
 
+#编译和安装
 make && make install
