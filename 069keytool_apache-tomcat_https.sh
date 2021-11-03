7843a2d92015525d343feff3ac7b143eea4c538c
thehejian <745544752@qq.com>
2021-11-03 16:21:03 +0800

    Update and rename 069keytool_apache-tomcat.sh to 069keytool_apache-tomcat_https.sh

--- 069keytool_apache-tomcat_https.sh
+++ 069keytool_apache-tomcat_https.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 keytool -genkeypair -alias tomcat -keyalg RSA -keystore /usr/local/tomcat/apache-tomcat-8.0.30/conf/keystore
-#siyou he zhengshu heyi
+#私钥和证书合一
+#私钥和证书的名称/usr/local/tomcat/apache-tomcat-8.0.30/conf/keystore
 
 sed -i '84d' /usr/local/tomcat/apache-tomcat-8.0.30/conf/server.xml
 sed -i '87d' /usr/local/tomcat/apache-tomcat-8.0.30/conf/server.xml
@@ -17,7 +18,7 @@ sleep 5
 ss -lnptu | grep java
 
 curl -k https://localhost_b:8443
-#localhost_b shi shangyige shezhi
+#localhost_b 是之前的虚拟主机设置
 #-k shi fangwen bu anquan de https
 #8443shi https de duankou; buneng moren shenglue
 
