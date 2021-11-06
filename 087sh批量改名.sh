0151643b693613da84b84628e781be39003767b6
thehejian <745544752@qq.com>
2021-11-06 15:05:16 +0800

    20211106 15:05:16

--- 087sh批量改名.sh
+++ 087sh批量改名.sh
@@ -5,7 +5,9 @@ path=~/cloud_linux/087mkdir批量改文件
 mkdir -p $path
 rm -rf $path/*
 touch $path/abc{1..10}hejian.txt
+echo "创建的文件"
 ls $path
+echo ""
 
 ####################——》改文件的名称hejian去掉
 #file=1hejian.txt
@@ -20,27 +22,39 @@ echo "改文件的名称hejian去掉"
 ls $path
 echo ""
 
-####################——》改文件的后缀.txt改成.TXT
+###########使用awk方法
+#cd $path
+#ls | awk -F "hejian" '{print "mv",$0,$1$2}' | bash
+#ls
+#-F "hejian" 以字符串为分割
+#print "mv",$0,$1$2 构造执行的命令管道传给bash
+
+###########使用rename方法
+#rename "hejian" "" $path/*
+
+####################——》改文件的后缀.txt改成.doc
 #file1=1hejian.txt
-#mv $file1 ${file1%.txt}.TXT
-#mv $file1 ${file1/$.txt/.TXT}
+#mv $file1 ${file1%txt}doc;
+#mv $file1 ${file1/$txt/doc};
 
 for file1 in $(ls $path/*)
 do
 mv $file1 ${file1/%txt/doc};
 done
 
-echo "改文件的后缀.txt改成.TXT"
+echo "改文件的后缀.txt改成.doc"
 ls $path
 echo ""
 
 ####################——》改文件的abc改成hehe
-file2=abc1.TXT
-mv file2 ${file2/#abc/hehe};
+#file2=abc1.doc
+#mv file2 ${file2/#abc/hehe};
+#mv file2 ${file2/abc/hehe};
+#不要加#不然不能成功
 #一定不能忘了分号
 for file2 in $(ls $path/*)
 do
-mv file2 ${file2/#abc/hehe};
+mv $file2 ${file2/abc/hehe};
 done
 echo "改文件的abc改成hehe"
 ls $path
