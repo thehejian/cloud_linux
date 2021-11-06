69f3b417ebeb3098cc731e682a92b78b113ba7d9
thehejian <745544752@qq.com>
2021-11-06 13:32:55 +0800

    Update 087sh批量改名.sh

--- 087sh批量改名.sh
+++ 087sh批量改名.sh
@@ -4,8 +4,9 @@
 mkdir -p ~/cloud_linux/087mkdir批量改文件
 rm -rf ~/cloud_linux/087mkdir批量改文件/*.txt
 touch {1..10}hejian.txt
+ls /cloud_linux/087mkdir批量改文件/*.txt
 
-
+####################——》改文件的名称hejian去掉
 #file=1hejian.txt
 #mv $file ${file%hejian*}.txt
 myls=$(ls ~/cloud_linux/087mkdir批量改文件/*.txt)
@@ -16,3 +17,15 @@ mv $file ${file%hejian*}.txt;
 done
 
 ls ~/cloud_linux/087mkdir批量改文件
+
+####################——》改文件的后缀.txt改成.TXT
+#file1=1hejian.txt
+#mv $file1 ${file1%.txt}.TXT
+#mv $file1 ${file1/$.txt/.TXT/}
+
+for file1 in $myls
+do
+mv $file1 ${file1/$.txt/.TXT/}
+done
+
+myls
