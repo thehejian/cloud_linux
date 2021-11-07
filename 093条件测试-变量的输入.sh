65c18ab0095f835f04c1816057538c2b6d0cd0da
thehejian <745544752@qq.com>
2021-11-07 15:56:17 +0800

    20211107 15:56:17

--- 093条件测试-变量的输入.sh
+++ 093条件测试-变量的输入.sh
@@ -16,3 +16,29 @@ read -p "请输入变量名称" name -t 5
 #&& || < > 可以用在[[]]中，其他不能用[] （转义后可以）
 #对于整数的数字运算，也可以使用(())
 
+
+###########——》test
+test -f file && echo "true" || echo "false"
+#false
+
+test ! -f file && echo "true" || echo "false"
+#ture
+
+#-n STRING
+#the length of STRING is nonzero
+#是否是数字
+
+#-z STRING
+#the length of STRING is zero
+#是否存在
+
+#-e 是否存在
+#-d 是否为目录
+#-f 是否为文件
+#-r 是否可读
+#-w 是否可写
+#-x 是否可执行
+#-L 符号连接
+#-c 是否字符设备
+#-b 是否块设备
+#-s 文件非空
