455be7469f637af6b3be4990889d8986194350d8
thehejian <745544752@qq.com>
2021-11-10 10:38:02 +0800

    Update 037host_login_v2_查找IP.sh

--- 037host_login_v2_查找IP.sh
+++ 037host_login_v2_查找IP.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-awk '/Failed password for root/{print $11}' /var/log/secure | awk '{ip[$1]++}END{for(i in ip){print ip[i],i}}' | awk '$1>3{print "入侵" $1 "次\t","ip 为" $2}' | sort -r
+awk '/Failed password for root/{print $11}' /var/log/secure | awk '{ip[$1]++}END{for(i in ip){print ip[i],i}}' | awk '$1>3{print "入侵" $1 "次\t","ip 为" $2}' | sort -r -n
+#sort  -n以数字方式排序，默认为文本
 #找到IP行
 #awk '{ip[$1]++}END{for(i in ip){print ip[i],i}}'
   #逐条执行ip，并自增计数；——》ip[i]是数组的序号，不是值——》进行自增统计；
