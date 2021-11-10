2267cd4674ada53952f446a875ccef168af4ede2
thehejian <745544752@qq.com>
2021-11-10 10:30:18 +0800

    Update 036host_login_查找远程访问的IP.sh

--- 036host_login_查找远程访问的IP.sh
+++ 036host_login_查找远程访问的IP.sh
@@ -3,7 +3,8 @@
 #awk '/Failed password for root/{a[$11]++}END{for(i in a){print "入侵 " a[i]" 次","\t IP为 " i}}' /nas/阿里云盘/001-脚本/043secure.log | sort -r
 #awk '/Failed password for root/{a[$11]++}END{for(i in a){print "入侵 " a[i] " 次\t","IP为 " i}}' /var/log/secure | sort -r
 
-x=$(awk '/Failed password for root/{ip[$11]++}END{for(i in ip){print ip[i]","i}}' /var/log/secure | sort -r)
+x=$(awk '/Failed password for root/{ip[$11]++}END{for(i in ip){print ip[i]","i}}' /var/log/secure | sort -r -n)
+#sort -n 以数字格式排序，默认是以文本格式
 #变量x是一个ip组（访问超过3次，从大到小排序）
 #awk的语法格式 /XX/找到XX这一行；
 #{YY}逐行执行/var/log/secure文件，执行命令为YY；
