3736578ab926ccc7dd838907447edbf2589c834b
thehejian <745544752@qq.com>
2021-11-13 10:43:44 +0800

    Update 105正则.sh

--- 105正则.sh
+++ 105正则.sh
@@ -97,9 +97,85 @@ grep -n '^[^[:upper:]]' 105regular_express.txt
 grep -n "\.$" 105regular_express.txt
 cat -An 105regular_express.txt | head -n 10 | tail -n 4
 grep -n "^$" 105regular_express.txt
+grep -n -v "^$" 105regular_express.txt | grep -v "^#"
+grep -n -v "^$" 105regular_express.txt | grep -v "^#" | cat -A
+grep -E -v -n "^$|^#" 105regular_express.txt
+egrep -nv "^$|^#" 105regular_express.txt
 
+################################################################正则的.和*
+. (小數點)：代表『一定有一個任意字元』的意思；
+* (星星號)：代表『重複前一個字元， 0 到無窮多次』的意思，為組合形態
 
+grep -n "g..d" 105regular_express.txt
+#grep -n "g??d" 105regular_express.txt
+#这样不对哦
 
+grep -n "o*" 105regular_express.txt
+#出现o 0次以及0次以上的内容，也就是全部页面
+
+grep -n "oo*" 105regular_express.txt
+#出现o （2-1）1次以及1次以上的内容
+grep -n "ooo*" 105regular_express.txt
+#2个oo及以上的内容
+
+grep -n "g*g" 105regular_express.txt
+#前面的g和后面的g， 前后任意出现一次，都显示
+#grep -n "g*" 105regular_express.txt
+#这两个都是显示全部；*前后没放内容则代表任意子串出现0次以及0次以上
+
+#grep -n "*g" 105regular_express.txt
+#grep -n "*gg" 105regular_express.txt
+#*代表重复前面字节，现在前面字节为空，匹配不到
+#重複 0 個或多個前面的 RE 字符
+
+ll *txt
+#注意区别，ll不支持正则；这里的*是通配符
+
+grep -n "go*g" 105regular_express.txt
+#gg之间0个及以上o
+grep -n "goo*g" 105regular_express.txt
+#gg之间1个及以上o
+grep -n "gooo*g" 105regular_express.txt
+#gg之间2个及以上o
+grep -n "g*g" 105regular_express.txt
+#后面g是固定的，所以搜索的内容必须的包含尾巴是g的内容
+#前面一个g代表，这个g出现0次或0次以上
+grep -n "g*" 105regular_express.txt
+#*后面没有东西，不匹配任何内容,类似下面这行，会显示全部内容
+#grep -n "" 105regular_express.txt
+#*前面的g代表出现0次或0次以上，类似上面的命令
+
+grep -n "*g" 105regular_express.txt
+#匹配不到任何内容
+#*前面不写代表全部内容类似下面内容
+#grep -n "" 105regular_express.txt
+#代表???
+
+grep -n "*g" 105regular_express.txt 
+#匹配不到任何内容；*前面必须放东西，哪怕是.
+grep -n "a*g" 105regular_express.txt 
+#g结尾，a出现0次以及以上
+grep -n ".*g" 105regular_express.txt 
+#从第一个子串开始，包含空格，g结尾，（任意字节）.出现0次及以上
+#找到所以g
+grep -n "..*g" 105regular_express.txt 
+#从第一个子串开始，包含空格，g结尾，（任意字节）.出现1次及以上；
+#g 这行会被过滤;*之前的任意字节没有出现
+grep -n "...*g" 105regular_express.txt 
+#从第一个子串开始，包含空格，g结尾，（任意字节）.出现2次及以上；
+#开始到g，不能少于2个字节；
+#g 这行会被过滤掉
+# g 空格+g 这行会被过滤掉
+grep -n ".*g" 105regular_express.txt
+#g结尾，任意子串出现0次及以上
+#有g的内容；选中的颜色是从开头到g出现那个字节全部显示
+grep -n "g.*" 105regular_express.txt 
+#g出现0次及以上
+#有g的内容；g开头到结尾
+grep -n "g*" 105regular_express.txt
+#全部内容；g颜色
+grep -n "gg*" 105regular_express.txt
+#g出现1个以上；g颜色
 
 
 
