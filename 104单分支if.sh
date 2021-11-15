9247ec858a4b05e5ac4af5b4830f295770d917f2
thehejian <745544752@qq.com>
2021-11-15 14:07:40 +0800

    Update 104单分支if.sh

--- 104单分支if.sh
+++ 104单分支if.sh
@@ -70,6 +70,40 @@ rm -rf tmp.txt;
 #不能这样搞，放在循环中会出错
 #最好搞个暂存文件，后面再删了,删除临时文件放循环外面吧；
 
+################################################################——》查看内存是否超100mb,发邮件
+cd /root/sdc/
+
+cat > 104mail_txt << "eof"
+虚拟机内存超了
+eof
+
+#yum -y install sendmail
+
+cat > 104mem_if.sh << "eof"
+#!/bin/bash
+mymem=$(free -m | grep "Mem" | awk '{print $4}')
+#free -m 按照mb显示
+#[ $mymem -le 100 ] && echo "chaole" || echo "ok"
+#比较时，方括号中不会出现>=\<=
+#数字判断，用-eq等
+#字符判断，不能用-eq等，只能用==和！=
+if [ $mymem -le 100 ]; then
+mail -s "标题" hejian10@huawei.com < 104mail_txt;
+#echo "test" | mail -s "mail_test" hejian10@huawei.com;
+else
+echo "ok"
+fi
+eof
+bash 104mem_if.sh
+
+( crontab -l; echo "*/3 * * * * /root/sdc/104mem_if.sh > /dev/null 2>&1" ) | crontab
+#每3分钟执行
+
+#添加一个crontab任务
+( crontab -l; echo "*/1 * * * * /root/sdc/104mem_if.sh > /dev/null 2>&1" ) | crontab
+#删除一个crontab任务
+crontab -l | grep -v "104mem_if.sh" | crontab
+
 
 
 
