3abd0add938aa3414d1e4bed835719c69551f497
thehejian <745544752@qq.com>
2021-11-16 19:16:15 +0800

    Update 120while和until.sh

--- 120while和until.sh
+++ 120while和until.sh
@@ -16,18 +16,35 @@ eof
 bash 120_while_1.sh
 
 ##########################################——》until
-#执行一次就退出；
 cat > 120_until_1.sh << "eof"
 #!/bin/bash
 read -p "input " a 
 #!/bin/bash
-until [ "$a" != "yes" -a "$a" != "YES" ]
+until [ "$a" == "yes" -a "$a" == "YES" ]
 do
-  echo -e "\n 你选对了"
+  echo -e "\n你丫只能输入这两个(yes|YES)";
+  sleep 1;
 done
-echo -e "\n你丫只能输入这两个(yes|YES)";
+echo -e "\n 你选对了"
 eof
 bash 120_until_1.sh
+
+##########################################——》0加到100(1)
+cat > 120_0-100.sh << "eof"
+#!/bin/bash
+a=0
+b=0
+while [ $a -lt 100 ]
+do
+  a=$((a+1))
+  b=$((b+a))
+done
+echo "$b"
+eof
+bash 120_0-100.sh
+
+
+
 
 
 
