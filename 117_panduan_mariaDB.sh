9905855679b6d623a3fb911fd375376c46e2c379
thehejian <745544752@qq.com>
2021-11-18 12:34:50 +0800

    20211118 12:34:50

--- 117_panduan_mariaDB.sh
+++ 117_panduan_mariaDB.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #mysql -u root -p '123456' > /dev/null 2>&1
-mysql -u root > /dev/null 2>&1
+#mysql -u root > /dev/null 2>&1
 #没设置密码，-p就省略掉吧
-[ $? -eq 0 ] && exit && echo "mysql已启动" || echo "mysql没启动"
+#得加个交互式命令，不然会卡死
+mysql -u root -e "show databases;" > /dev/null 2>&1
+[ $? -eq 0 ] && echo "mysql已启动" || echo "mysql没启动"
