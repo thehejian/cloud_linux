8ca60895a4f8e0e2e7275a0bd927e725cf878f7b
thehejian <745544752@qq.com>
2021-11-18 12:07:59 +0800

    Update 117双分支-if.sh

--- 117双分支-if.sh
+++ 117双分支-if.sh
@@ -171,11 +171,15 @@ fi
 eof
 bash 117_panduan_nginx.sh
 
- 
-
-
-
-
+####################################——》判断mariaDb
+cat > 117_panduan_mariaDB.sh << "eof" 
+#!/bin/bash
+#mysql -u root -p '123456' > /dev/null 2>&1
+mysql -u root > /dev/null 2>&1
+#没设置密码，-p就省略掉吧
+[ $? -eq 0 ] && echo "mysql已启动" || echo "mysql没启动"
+eof
+bash 117_panduan_mariaDB.sh
 
 
 
