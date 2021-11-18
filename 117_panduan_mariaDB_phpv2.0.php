6ca76eb06e0cb31cad719d5fba708b62a1ec6054
thehejian <745544752@qq.com>
2021-11-18 12:56:58 +0800

    20211118 12:56:58

--- 117_panduan_mariaDB_phpv2.0.php
+++ 117_panduan_mariaDB_phpv2.0.php
@@ -12,6 +12,3 @@ catch(PDOException $e)
     echo $e->getMessage(); 
 } 
 ?>
-#php 117_panduan_mariaDB_phpv2.0.php
-/bin/cp -rf 117_panduan_mariaDB_phpv2.0.php /usr/local/nginx/html/
-curl 1.116.26.230/117_panduan_mariaDB_phpv2.0.php
