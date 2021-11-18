d4ed0a42e0c3017d5b24b002daaa9a14dc2577aa
thehejian <745544752@qq.com>
2021-11-18 12:18:39 +0800

    20211118 12:18:39

--- 117_panduan_mariaDB.sh
+++ 117_panduan_mariaDB.sh
@@ -2,4 +2,4 @@
 #mysql -u root -p '123456' > /dev/null 2>&1
 mysql -u root > /dev/null 2>&1
 #没设置密码，-p就省略掉吧
-[ $? -eq 0 ] && echo "mysql已" && exit || echo "mysql没启动"
+[ $? -eq 0 ] && exit && echo "mysql已启动" || echo "mysql没启动"
