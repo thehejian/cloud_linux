bbddc2bad6ab05ea739edc9b5b5419c41d838bcc
thehejian <745544752@qq.com>
2021-11-19 13:51:11 +0800

    20211119 13:51:11

--- 117_web_nmap.sh
+++ 117_web_nmap.sh
@@ -1,6 +1,6 @@
 echo -e "\nnmap 162.166.94.62 -p 80 | grep open | wc -l0"
+mynmap=$(nmap 162.166.94.62 -p 80 | grep open | wc -l)
 [ $mynmap -eq 1 ] && echo -e "\nwebserver(nginx/apache) is UP" || echo -e "\nwebserver(nginx/apache) is DOWN"
 #nmap 1.116.26.230 -p 80 
 #$?不行哈
-mynmap=$(nmap 162.166.94.62 -p 80 | grep open | wc -l)
 #1
