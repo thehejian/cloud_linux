f03232ab0689634a9614211a54b384a5bcdb6429
thehejian <745544752@qq.com>
2021-11-20 10:50:30 +0800

    Update 117-双分支-if.sh

--- 117-双分支-if.sh
+++ 117-双分支-if.sh
@@ -293,6 +293,21 @@ ps -ef | grep nginx | grep -v grep &> /dev/null
 eof
 bash 117_web_ps.sh
 
+###############################################################——》#web2.0
+cat > 117_web2.0.sh << "eof"
+source /etc/init.d/functions
+read -p "请输入命令，（start|stop）" mycmd
+#调用本地函数库
+if [ "$mycmd" == "start" ]; then 
+    action "nginx is starting" /bin/true
+elif [ "$mycmd" == "stop" ]; then    
+    action "nginx is DOWN" /bin/false
+else
+    action "你丫只能输入（start|stop）bash $0" /bin/false
+fi
+eof
+bash 117_web2.0.sh
+
 
 
 
