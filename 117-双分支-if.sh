8ee36baec7257a862c732ec656f8dc2d9c4b06e6
thehejian <745544752@qq.com>
2021-11-20 16:45:11 +0800

    Update 117-双分支-if.sh

--- 117-双分支-if.sh
+++ 117-双分支-if.sh
@@ -370,10 +370,27 @@ done
 eof
 bash 117_建立原始文件_path.sh
 
-
-
-
-
+###############################################################——》#检查站点目录是否修改v3.0
+cat > 117_jianchav3.0.sh << "eof"
+#千万不要少个<号
+#!/bin/bash
+rm -rf 117_path_ori.txt
+mypath=/usr/local/nginx
+myfind=$(find "$mypath" -type f -name "*.conf")
+#建立指纹，把所有文件放一起;字符数量加文件名
+for i in $myfind
+do
+	wc -L $i >> 117_path_ori.txt
+done
+source /etc/init.d/functions
+for j in $myfind
+do
+	mynow=$(cat "$j" | wc -L);
+	myori=$(grep "$j" 117_path_ori.txt | awk '{print $1}');
+	[  $mynow -eq $myori ] && action "$j  文件没变" /bin/true || action "$j  文件变了" /bin/fasle;
+done
+eof
+bash 117_jianchav3.0.sh
 
 
 
