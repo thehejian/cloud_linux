9e0255dce77f890dedc1eceafd9b38e540809041
thehejian <745544752@qq.com>
2021-11-20 15:07:40 +0800

    Update 117-双分支-if.sh

--- 117-双分支-if.sh
+++ 117-双分支-if.sh
@@ -323,16 +323,19 @@ done
 
 source /etc/init.d/functions
 mydiff=$(diff 117_md5sum_nginx_"$mydate".txt 117_md5sum_nginx_ori.txt)
-[ -z "$mydiff" ] && action "$path配置没变化" /bin/true || action "$path配置文件变化了" /bin/false; echo -e "详情如下:\n$mydiff"
-
+[ -z "$mydiff" ] && action "$path配置没变化" /bin/true || {
+	action "$path配置文件变化了" /bin/false
+	echo -e "详情如下:\n$mydiff"
+}
 eof
 bash 117_zhandian站点目录是否修改.sh
 
-################——》##建立默认文件
+################——》##建立默认文件,这一步在上一步之前
 cat > 117_zhandian站点目录是否修改_path.sh << "eof"
 #建立默认文件
 mypath=/usr/local/nginx
 myfind=$(find "$mypath" -type f -name "*.conf")
+#rm -rf 117_zhandian站点目录是否修改_path.sh
 for i in $myfind
 #遍历全部的配置文件
 do
@@ -341,7 +344,30 @@ done
 eof
 bash 117_zhandian站点目录是否修改_path.sh
 
+###############################################################——》#检查站点目录是否修改v2.0
+cat > 117_zhandian站点目录是否修改v2.0.sh << "eof"
+#检查站点目录是否修改
+mypath=/usr/local/nginx
+mydate=$(date +%Y%m%d)
+myfind=$(find $mypath -type f -name "*.conf")
+#通过文件目录判断，不够严谨哈
+rm -rf 117_log_nginx_"$mydate".txt
+for i in $myfind
+#遍历全部的配置文件
+do
+	#echo "$myfind" > 117_log_nginx_ori.txt
+	echo "$myfind" > 117_log_nginx_"$mydate".txt
+	#md5sum $i >> 117_md5sum_nginx_"$mydate".txt
+done
 
+source /etc/init.d/functions
+mydiff=$(diff 117_log_nginx_"$mydate".txt 117_log_nginx_ori.txt)
+[ -z "$mydiff" ] && action "$path配置没变化" /bin/true || {
+	action "$path配置文件变化了" /bin/false
+	echo -e "详情如下:\n$mydiff"
+}
+eof
+bash 117_zhandian站点目录是否修改v2.0.sh
 
 
 
