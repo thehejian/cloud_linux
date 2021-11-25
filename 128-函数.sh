34e52862ae2d868d9b6197c8518dad2b122a803d
thehejian <745544752@qq.com>
2021-11-25 13:21:48 +0800

    Update 128-函数.sh

--- 128-函数.sh
+++ 128-函数.sh
@@ -79,6 +79,119 @@ COMMAND    PID USER   FD   TYPE    DEVICE SIZE/OFF NODE NAME
 rsync   161772 root    5u  IPv4 171711325      0t0  TCP *:rsync (LISTEN)
 rsync   161772 root    6u  IPv6 171711326      0t0  TCP *:rsync (LISTEN)
 
+#################################################################################################################
+cat > 128_05function.sh << "eof"
+source /etc/init.d/functions
+fun_rsync () {
+case $1 in
+  start | 1)
+    pkill rsync
+    sleep 2
+    rsync --daemon
+    [ $? -eq 0 ] && action "启动成功" /bin/true || action "启动失败" /bin/false
+  ;;
+  stop | 2)
+    pkill rsync
+    sleep 1
+    ps -ef | grep -v grep | grep rsync &> /dev/null
+    [ $? -eq 0 ] && action "关闭成功" /bin/true || action "关闭失败" /bin/false
+    #这里搞不定，好像又可以了
+    #[ $? -eq 0 ] && action "关闭成功" /bin/true || action "关闭失败" /bin/false
+    #关闭时再关闭，会报错
+    #lsof -i :873 &> /dev/null
+  ;;
+  restart | 3)
+    pkill rsync
+    sleep 2
+    rsync --daemon
+    [ $? -eq 0 ] && action "重启成功" /bin/true || action "重启失败" /bin/false
+  ;;
+  *)
+    action "Usage: $0 {(start | 1)(stop | 2)(restart | 3)}" /bin/false
+    ;;
+esac
+}
+eof
+####
+cat > 128_06_rsync.sh << "eof"
+read -p "请输入指令 (start | 1)(stop | 2)(restart | 3) " mycmd
+source 128_05function.sh
+fun_rsync $mycmd
+eof
+bash 128_06_rsync.sh
+
+#################################################################################################################
+cat > 128_07function.sh << "eof"
+source /etc/init.d/functions
+start () {
+    ps -ef | grep -v grep | grep rsync &> /dev/null
+    if [ $? -eq 0 ]; then
+        action "已启动" /bin/true
+    else
+        rsync --daemon
+        action "启动成功" /bin/true
+    fi
+}
+stop () {
+    ps -ef | grep -v grep | grep rsync &> /dev/null
+    [ $? -eq 0 ] && {
+    pkill rsync
+    action "关闭成功" /bin/true
+    } || action "已关闭" /bin/true
+}
+restart () {
+    pkill rsync
+    sleep 2
+    rsync --daemon
+    [ $? -eq 0 ] && action "重启成功" /bin/true || action "重启失败" /bin/false
+}
+eof
+###########################################################################
+cat > 128_08_rsync.sh << "eof"
+read -p "请输入指令 (start | 1)(stop | 2)(restart | 3) " mycmd
+source 128_07function.sh
+case $mycmd in
+    start | 1)
+    start
+    ;;
+    stop | 2)
+    stop
+    ;;
+    restart | 3)
+    restart
+    ;;
+    *)
+    action "Usage: $0 {(start | 1)(stop | 2)(restart | 3)}" /bin/false
+    ;;
+esac
+eof
+bash 128_08_rsync.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
