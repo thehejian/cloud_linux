9bc2acb4a60cba67a0fe137f43c1fbf8456d9079
thehejian <745544752@qq.com>
2022-02-07 19:15:22 +0800

    Update 001git_hub.sh

--- tedu/001git_hub.sh
+++ tedu/001git_hub.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-#这个脚本是首次对接github和同步数据到github的origin仓库
+#这个脚本是对接github和同步数据到github的origin仓库；首次创建、克隆等
+#参考 https://blog.csdn.net/majinggogogo/article/details/81152938
 
+#设置邮箱变量
 mail=745544752@qq.com
+
+#安装git
+yum -y install git
+
+#########################################################################################——》创建公钥
 ssh-keygen -t ed25519 -C "$mail"
 
+#########################################################################################——》复制公钥到github
 #需要把公钥粘贴到github上；https://github.com/settings/keys
-#cp /root/.ssh/id_ed25519.pub /cloudnas/cloudnas/CloudDrive/阿里云盘/001-脚本/
-#公钥（证书能复制出去就行）
-#将公钥复制到 nginx 的 html 目录下;
-#详细请参考 002ssh_copy_id.sh
-#参考
-#https://blog.csdn.net/majinggogogo/article/details/81152938
+#github可以添加多个公钥
 
+#########################################################################################——》测试能否连接github
 ssh -T git@github.com
 #测试能否连接github
 
+#########################——》用来区分用户，单人使用可不关注
 git config --global user.name "thehejian"
 git config --global user.email "745544752@qq.com"
 #配置 名称 和 邮箱
-#########################################################################################场景一：本地创建文件，并上传到github的origin仓库
+
+#########################——》场景一：本地克隆
+mkdir -p ~/cloud_linux
+cd ~/cloud_linux
+git clone git@github.com:thehejian/cloud_linux.git
+
+#########################——》场景二：本地创建文件，并上传到github的origin仓库
 cd ~
 #默认放到家目录
 #不能放到云盘上，上传不了
@@ -55,11 +66,6 @@ git push -u origin main
 #提交所有变更到github
 #第一次加上 -u 
 
-#########################################################################################场景二：本地克隆
-mkdir -p ~/cloud_linux
-cd ~/cloud_linux
-git clone origin git@github.com:thehejian/cloud_linux.git
-
-##########################################################################################场景三：日常操作
+#########################——》场景三：日常操作
 #请参考脚本000upgate_git.sh
 bash 000upgate_git.sh
